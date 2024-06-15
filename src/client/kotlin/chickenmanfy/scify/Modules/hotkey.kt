73386e7ab2b4ba2b1f8c1fb4b38a96145d3cf925
desyncfy <chickenmanfy@proton.me>
2024-06-15 13:34:27 -0700

    Update hotkey.kt

--- src/client/kotlin/chickenmanfy/scify/Modules/hotkey.kt
+++ src/client/kotlin/chickenmanfy/scify/Modules/hotkey.kt
@@ -1,14 +1,10 @@
 package chickenmanfy.scify.Modules
 
-import io.netty.buffer.ByteBuf
 import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents
 import net.fabricmc.fabric.api.client.keybinding.v1.KeyBindingHelper
 import net.minecraft.client.MinecraftClient
 import net.minecraft.client.option.KeyBinding
 import net.minecraft.client.util.InputUtil
-import net.minecraft.network.PacketByteBuf
-import net.minecraft.network.packet.c2s.play.ChatMessageC2SPacket
-import net.minecraft.text.Text
 import org.lwjgl.glfw.GLFW
 
 
@@ -40,7 +36,7 @@ class hotkey {
                 "SciFy"
             )
         )
-        ClientTickEvents.END_CLIENT_TICK.register(ClientTickEvents.EndTick { client: MinecraftClient ->
+        ClientTickEvents.END_CLIENT_TICK.register(ClientTickEvents.EndTick {
             while (warp.wasPressed()) {
                 MinecraftClient.getInstance().networkHandler!!.sendChatCommand("warp")
             }
