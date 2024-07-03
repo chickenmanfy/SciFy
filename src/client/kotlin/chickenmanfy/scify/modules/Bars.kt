dd4cf8c5a7e7c2c72394cc78ad9c0e423efdcaee
desyncfy <chickenmanfy@proton.me>
2024-07-03 08:56:40 -0700

    This isn't working delete it

--- src/client/kotlin/chickenmanfy/scify/modules/Bars.kt
+++ src/client/kotlin/chickenmanfy/scify/modules/Bars.kt
@@ -1,9 +1,7 @@
 package chickenmanfy.scify.modules
 
-import chickenmanfy.scify.SciFyClient.debugUser
 import com.mojang.blaze3d.systems.RenderSystem
 import net.fabricmc.fabric.api.client.rendering.v1.HudRenderCallback
-import net.minecraft.client.MinecraftClient
 import net.minecraft.client.gui.DrawContext
 import net.minecraft.client.render.*
 import net.minecraft.util.Identifier
@@ -19,10 +17,6 @@ class Bars {
             val tessellator: Tessellator = Tessellator.getInstance()
             val buffer: BufferBuilder = tessellator.buffer
             val positionMatrix = drawContext?.matrices?.peek()?.positionMatrix
-            if (MinecraftClient.getInstance().networkHandler?.serverInfo?.address == "dungeonfy.minehut.gg" ||
-                MinecraftClient.getInstance().networkHandler?.serverInfo?.address == "51.222.121.148:25599" ||
-                MinecraftClient.getInstance().networkHandler?.serverInfo?.address == "minehut.com" ||  // if you join minehut.com and type /join dungeonfy
-                MinecraftClient.getInstance().player?.name?.string == debugUser) {
                 if (barsToggle) {
                     buffer.begin(VertexFormat.DrawMode.QUADS, VertexFormats.POSITION_COLOR_TEXTURE)
                     buffer.vertex(positionMatrix, 20F, 20F, 0F).color(1f, 1f, 1f, 1f).texture(0f, 0f).next()
@@ -31,12 +25,11 @@ class Bars {
                     buffer.vertex(positionMatrix, 60F, 20F, 0F).color(1f, 1f, 1f, 1f).texture(1f, 0f).next()
 
                     RenderSystem.setShader { GameRenderer.getPositionColorTexProgram() }
-                    RenderSystem.setShaderTexture(0, Identifier("scify", "scify.png"))
+                    RenderSystem.setShaderTexture(0, Identifier("scify", "/Health And Mana/Health/Vials_0001.png"))
                     RenderSystem.setShaderColor(1f, 1f, 1f, 1f)
 
                     tessellator.draw()
                 }
-            }
         })
     }
 }
